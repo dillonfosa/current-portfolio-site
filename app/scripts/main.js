38be9e30e579d31ce962a8603d04c1407f93ca46
Dillon Fosa <dillonfosa@gmail.com>
2017-03-02 17:06:50 -0500

    added content to contact and footer

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,14 @@
 $( document ).ready(function(){
     
-    $('.button-collapse').sideNav();
+   
+    
+     $('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 300
+      edge: 'right', // Choose the horizontal origin
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true // Choose whether you can drag to open on touch screens
+    }
+  );
     
      new WOW().init();
         
@@ -218,3 +226,5 @@ g.append('text')
   .attr('text-anchor', 'middle')
   .attr('dy', '.35em')
   .text(text);
+
+
