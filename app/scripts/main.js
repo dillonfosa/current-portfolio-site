cd76cc6e3f50bc655ecaf99f8aa4324ca7c51ed2
Dillon Fosa <dillonfosa@gmail.com>
2017-03-03 20:01:40 -0500

    added utopian-hotels details and adjusted footer nav

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -228,51 +228,10 @@ g.append('text')
   .text(text);
 
 
-//quote slider
-
-var theSlide = 2,
-    numSlides = 4,
-    frequency = 5000,
-    lastButton = "#quote-1";
-
-var slide = function(){
-    var	currentSlide = "#quote-"+theSlide+"-content",
-				lastSlide = lastButton+"-content",
-        currentButton = "#quote-"+theSlide;
-
-    $(".slider-quote").removeClass("in-focus");
-    $(".quote-control").removeClass("selected");
-    $(currentSlide).addClass("in-focus");
-    $(currentButton).addClass("selected");
 
 
-    lastButton = currentButton;
-    if(theSlide < numSlides){
-        theSlide += 1;
-    }
-    else{
-        theSlide = 1;
-    }
-};
+//parallex container js
 
-var interval = setInterval(slide, frequency);
-
-$(function(){
-
-    $("div#quote-slider").mouseover(function(){
-        clearInterval(interval);
-    }).mouseout(function(){
-        interval = setInterval(slide, frequency);
+$(document).ready(function(){
+      $('.parallax').parallax();
     });
-  
-  	$(".quote-control").click(function(e){
-      	clearInterval(interval);
-        $(".slider-quote").removeClass("in-focus");
-    		$(".quote-control").removeClass("selected");
-      	var quoteSlider = "#"+$(this).attr("id")+"-content";
-      	$(quoteSlider).addClass("in-focus");
-      	$(this).addClass("selected");
-      	$("div#quote-slider").unbind('mouseover mouseout');
-    });
-
-});
