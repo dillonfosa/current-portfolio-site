873b7ea7809c3d51c463a80fd4b4df3f308f8cc7
Dillon Fosa <dillonfosa@gmail.com>
2019-07-28 11:58:33 -0400

    Changed resume file and removed various timelines.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -8,8 +8,7 @@ $( document ).ready(function(){
       edge: 'right', // Choose the horizontal origin
       closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
       draggable: true // Choose whether you can drag to open on touch screens
-    }
-  );
+    } );
     
      new WOW().init();
         
